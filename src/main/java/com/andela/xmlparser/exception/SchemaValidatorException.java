c43bc7aae13c727ffdc8483fc25b9235e8d16adf
Sudarshana Reddy <sud8640@gmail.com>
2023-02-13 12:07:54 +0530

    exception class committed

---
+++ src/main/java/com/andela/xmlparser/exception/SchemaValidatorException.java
@@ -0,0 +1,8 @@
+package com.andela.xmlparser.exception;
+
+public class SchemaValidatorException extends RuntimeException {
+
+    public SchemaValidatorException(String message) {
+        super(message);
+    }
+}
